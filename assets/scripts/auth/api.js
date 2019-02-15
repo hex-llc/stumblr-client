722c71a6c9f26111bd2ebdba1528ac9aaf102497
rogerdunnGA <rogerdunn@Gmail.com>
2019-02-15 15:27:28 -0500

    Add authentication for app.js events.js and api.js
    -User will now be able to sign-up, sign-in, change pw, sign-out.
    -Still have ui.js left to do
    -Using promise and .then and .catch to send to ui.js

---
+++ assets/scripts/auth/api.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const config = require('../config.js')
+const store = require('../store.js')
+
+const signUp = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: POST,
+    data: data
+  })
+}
+
+const signIn = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-in',
+    method: POST,
+    data: data
+  })
+}
+
+const changePw = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/change-password',
+    method: PATCH,
+    header: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: data
+  })
+}
+
+const signOut = function () {
+  return $.ajax({
+    url: config.apiUrl + '/sign-out',
+    method: DESTROY,
+    header: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  changePw,
+  signOut
+}
